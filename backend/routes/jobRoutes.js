f42691a9620f89a1b90095adf823d4a56631125e
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-16 23:25:12 +0530

    Commit description:
    ' 1. New Route Definition
    ' - Route: `GET /api/v1/job/get/:id`
    '   - This route is protected by middleware and is accessible only to authenticated users.
    '   - The route is designed to handle GET requests that include a job ID as a URL parameter.
    '   - The route utilizes the `protectedRoute` middleware to ensure that only authorized users can access the resource.
    
    ' 2. Controller Functionality
    ' - `getJobById`: The controller function linked to this route is responsible for retrieving a job document from the database based on the provided job ID.
    '   Key operations include:
    '   - Extracting the `jobId` from the request parameters.
    '   - Fetching the job using Mongoose's `findById` method, with the associated `applications` populated for comprehensive data retrieval.
    '   - Handling scenarios where the job is not found by returning a JSON response with an appropriate error message.
    '   - Returning the job details in the response if the job is found.
    
    ' 3. Error Handling
    ' - The `getJobById` function is wrapped in a try-catch block to manage any potential errors during the database query.
    '   - On failure, it logs the error message and returns a JSON response with a `success: false` status and the error message.

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { postJob, getAllJobs } = require('../controllers/jobControllers');
+const { postJob, getAllJobs, getJobById } = require('../controllers/jobControllers');
 const protectedRoute = require('../middleware/protectedRoute')
 
 
 router.route('/post').post(protectedRoute, postJob)
 router.route('/get').get(protectedRoute, getAllJobs);
+router.route('/get/:id').get(protectedRoute, getJobById);
 
 
 module.exports = router;
