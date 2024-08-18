119a8aa6113d8b3496ac579dadfa3eed230639a1
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-18 08:49:23 +0530

    Commit description: ' This React component implements a toggle switch for light and dark modes using Chakra UI's color mode system. ' It utilizes React Icons for visual representation (IoSunny for light mode, FaMoon for dark mode). ' The component also integrates with Framer Motion for smooth transitions between states. ' The `Mode` component takes `top`, `right`, `width`, and `height` as props for positioning and sizing the toggle switch. ' The `useEffect` hook is used to synchronize the toggle switch state with the current color mode.

---
+++ frontend/src/Mode(LIight and dark)/Mode.jsx
@@ -0,0 +1,54 @@
+import { useColorMode } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react'
+import { IoSunny } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
+import '../index.css'
+import { motion } from "framer-motion";
+
+
+const Mode = ({ top, right, width, height }) => {
+    const { colorMode, toggleColorMode } = useColorMode()
+    const [isOn, setIsOn] = useState(false);
+    const toggleSwitch = () => setIsOn(!isOn);
+
+    const bothToggle = () => {
+        toggleSwitch();
+        toggleColorMode();
+    }
+
+    useEffect(() => {
+        if (colorMode === "dark") {
+            setIsOn(true);
+        }
+        else {
+            setIsOn(false)
+        }
+    }, [colorMode])
+
+    const styles = {
+        top: top,
+        right: right,
+        width: width,
+        height: height
+    }
+    return (
+        <>
+            <div className="switch" style={styles} data-ison={isOn} onClick={bothToggle}>
+                <motion.div layout transition={spring}>
+                    {
+                        !isOn ? <IoSunny /> : <FaMoon />
+                    }
+                </motion.div>
+            </div>
+        </>
+    )
+}
+
+const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30
+};
+
+
+export default Mode
