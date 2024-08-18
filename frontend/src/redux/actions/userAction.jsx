8a42d62000cba9835c49fcd1b54066000de4414e
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-19 00:22:39 +0530

    commit descrition Add login action for user authentication
    - Implemented `login` action to handle user login.
    - Added `LOGIN_REQUEST`, `LOGIN_SUCEESS`, and `LOGIN_FAIL` action types.
    - Managed user authentication and error handling for login requests.

--- frontend/src/redux/actions/userAction.jsx
+++ frontend/src/redux/actions/userAction.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import {
     CLEAR_ERRORS,
+    LOGIN_FAIL,
+    LOGIN_REQUEST,
+    LOGIN_SUCEESS,
     REGISTER_USER_FAIL,
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCEESS,
 } from '../constants/userConstants';
 
 
+// REGISTER
 const register = (userData) => async (dispatch) => {
     try {
         dispatch({ type: REGISTER_USER_REQUEST })
@@ -17,7 +21,6 @@ const register = (userData) => async (dispatch) => {
             userData, // data
             config,
         );
-        console.log(response.data)
 
         if (response.data.success) {
             dispatch({
@@ -42,9 +45,42 @@ const register = (userData) => async (dispatch) => {
     }
 }
 
+//LOGIN
+const login = (email, password, role) => async (dispatch) => {
+    try {
+        dispatch({ type: LOGIN_REQUEST });
+        const response = await axios.post(
+            '/api/v1/user/login',
+            { email, password, role },
+            { headers: { "Content-Type": "application/json" } }
+        );
+
+        if (response.data.success) {
+            dispatch({
+                type: LOGIN_SUCEESS,
+                payload: response.data,
+            })
+        }
+        else {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: response.data.message
+            });
+        }
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+
+    }
+}
+
 // clearing Error
 const clearError = () => async (dispatch) => {
     dispatch({ type: CLEAR_ERRORS });
 }
 
-export { register, clearError };
+export { register, login, clearError };
