c20c2f2e2ca50103d9cf860cb54179f2886c0b01
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-19 00:24:20 +0530

    commit description Implement Login Functionality
    Added login form with email, password, and role fields using Chakra UI.
    Integrated login action dispatch on form submission.
    Managed form state and displayed error/success messages with react-hot-toast.
    Redirected authenticated users to the home page and handled errors.

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -18,12 +18,40 @@ import {
     Center,
     Avatar,
 } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { clearError, login } from '../../redux/actions/userAction';
+import toast from 'react-hot-toast';
 
 export default function Login() {
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { error, loading, isAuthenticated, message } = useSelector((state) => state.user);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [role, setRole] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        dispatch(login(email, password, role));
+    }
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            dispatch(clearError());
+        }
+        if (isAuthenticated) {
+            navigate('/');
+            // Cookies.set('token', token); // लॉगिन के बाद कुकीज़ में टोकन सेट करें
+        }
+        if (message) {
+            toast.success(message);
+        }
+    }, [dispatch, error, message, isAuthenticated, navigate]);
 
     return (
         <Flex
@@ -38,56 +66,80 @@ export default function Login() {
                         Sign in
                     </Heading>
                 </Stack>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.900')}
-                    boxShadow={'lg'}
-                    p={8}
-                    w={{
-                        lg: "400px",
-                        sm: '100%',     // Small screens like tablets
-                        md: '100%',     // Medium screens like laptops
-                    }}
-                >
-                    <Stack spacing={4}>
+                <form onSubmit={handleLogin}>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.900')}
+                        boxShadow={'lg'}
+                        p={8}
+                        w={{
+                            lg: "400px",
+                            sm: '100%',     // Small screens like tablets
+                            md: '100%',     // Medium screens like laptops
+                        }}
+                    >
+                        <Stack spacing={4}>
 
-                        <FormControl id="email" isRequired>
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
-                        </FormControl>
-                        <FormControl id="password" isRequired>
-                            <FormLabel>Password</FormLabel>
-                            <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
-                                <InputRightElement h={'full'}>
-                                    <Button
-                                        variant={'ghost'}
-                                        onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </FormControl>
+                            <FormControl id="email" isRequired>
+                                <FormLabel>Email address</FormLabel>
+                                <Input type="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <InputGroup>
+                                    <Input type={showPassword ? 'text' : 'password'}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={password}
+                                    />
+                                    <InputRightElement h={'full'}>
+                                        <Button
+                                            variant={'ghost'}
+                                            onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <HStack>
+                                <FormControl isRequired>
+                                    <FormLabel>Role</FormLabel>
+                                    <Select
+                                        placeholder='Role'
+                                        name="role"
+                                        onChange={(e) => setRole(e.target.value)}
+                                        value={role}
+                                    >
+                                        <option value='student'>Student</option>
+                                        <option value='recruiter'>Recruiter</option>
+                                    </Select>
 
-                        <Stack spacing={10} pt={2}>
-                            <Button
-                                loadingText="Submitting"
-                                size="lg"
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}>
-                                Sign in
-                            </Button>
+                                </FormControl>
+                            </HStack>
+                            <Stack spacing={10} pt={2}>
+                                <Button
+                                    type='submit'
+                                    isLoading={loading}
+                                    loadingText="Submitting"
+                                    size="lg"
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}>
+                                    Sign in
+                                </Button>
+                            </Stack>
+                            <Stack pt={6}>
+                                <Text align={'center'}>
+                                    Don't have account? <Link to={'/register'}>sign up</Link>
+                                </Text>
+                            </Stack>
                         </Stack>
-                        <Stack pt={6}>
-                            <Text align={'center'}>
-                                Don't have account? <Link to={'/register'}>sign up</Link>
-                            </Text>
-                        </Stack>
-                    </Stack>
-                </Box>
+                    </Box>
+                </form>
             </Stack>
         </Flex>
     )
