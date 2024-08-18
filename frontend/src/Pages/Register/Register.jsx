93e3b5ca21c51069326f15787b6b75b60ce59d31
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-18 23:50:48 +0530

    commit description Implement user registration form with image preview
    - Integrated user registration form with Redux actions for `register` and `clearError`.
    - Added image preview functionality using a custom hook.
    - Managed form state and submission using `FormData` to handle file uploads.
    - Implemented redirection and toast notifications for success and error handling.

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -18,103 +18,186 @@ import {
     Center,
     Avatar,
 } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import useImagePreview from '../../CustomHook/useImagePreview'
+import { clearError, register } from '../../redux/actions/userAction';
+import toast from 'react-hot-toast';
 
 export default function Register() {
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { error, loading, isAuthenticated, message } = useSelector((state) => state.user);
+    const { handleImageOnChange, imageUrl } = useImagePreview();
+    const [inputs, setInputs] = useState({
+        fullname: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+        role: "",
+    });
+
+    const imageRef = useRef(null);
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setInputs({ ...inputs, [name]: value });
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const userData = new FormData();
+        userData.set('fullname', inputs.fullname);
+        userData.set('email', inputs.email);
+        userData.set('password', inputs.password);
+        userData.set('role', inputs.role);
+        userData.set('phoneNumber', inputs.phoneNumber);
+
+        // Ensure `imageUrl` is a valid File or Blob
+        if (imageUrl) {
+            userData.append('profilePic', imageUrl);
+        }
+
+
+        dispatch(register(userData));
+    };
+
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            dispatch(clearError());
+        }
+        if (isAuthenticated) {
+            navigate('/login');
+            // Cookies.set('token', token); // लॉगिन के बाद कुकीज़ में टोकन सेट करें
+        }
+        if (message) {
+            toast.success(message);
+        }
+    }, [dispatch, error, message, isAuthenticated, navigate]);
 
     return (
-        <Flex
-            align={'center'}
-            justify={'center'}
-        >
-            <Stack spacing={7} mx={'auto'} maxW={'lg'} py={12} px={6}
-            >
+        <Flex align={'center'} justify={'center'}>
+            <Stack spacing={7} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
                     <Heading fontSize={'4xl'} textAlign={'center'}>
                         Sign up
                     </Heading>
                 </Stack>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.900')}
-                    boxShadow={'lg'}
-                    p={8}>
-                    <Stack spacing={4}>
-                        <HStack>
-                            <Box>
-                                <FormControl id="firstName" isRequired>
-                                    <FormLabel>Full Name</FormLabel>
-                                    <Input type="text" />
-                                </FormControl>
-                            </Box>
-                            <Box>
-                                <FormControl id="lastName" isRequired>
-                                    <FormLabel>Phone Number</FormLabel>
-                                    <Input type="number" />
-                                </FormControl>
-                            </Box>
-                        </HStack>
-                        <FormControl id="email" isRequired>
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
-                        </FormControl>
-                        <FormControl id="password" isRequired>
-                            <FormLabel>Password</FormLabel>
-                            <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
-                                <InputRightElement h={'full'}>
-                                    <Button
-                                        variant={'ghost'}
-                                        onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </FormControl>
-                        <HStack>
-                            <FormControl isRequired>
-                                <FormLabel>Role</FormLabel>
-                                <Select placeholder='Role'>
-                                    <option value='student'>Student</option>
-                                    <option value='recruiter'>Recruiter</option>
-                                </Select>
+                <form onSubmit={handleRegister}>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.900')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack spacing={4}>
+                            <HStack>
+                                <Box>
+                                    <FormControl id="firstName" isRequired>
+                                        <FormLabel>Full Name</FormLabel>
+                                        <Input type="text"
+                                            name="fullname"
+                                            onChange={handleOnChange}
+                                            value={inputs.fullname}
+                                        />
+                                    </FormControl>
+                                </Box>
+                                <Box>
+                                    <FormControl id="lastName" isRequired>
+                                        <FormLabel>Phone Number</FormLabel>
+                                        <Input type="number"
+                                            name="phoneNumber"
+                                            onChange={handleOnChange}
+                                            value={inputs.phoneNumber}
+                                        />
+                                    </FormControl>
+                                </Box>
+                            </HStack>
+                            <FormControl id="email" isRequired>
+                                <FormLabel>Email address</FormLabel>
+                                <Input type="email"
+                                    name="email"
+                                    onChange={handleOnChange}
+                                    value={inputs.email}
+                                />
                             </FormControl>
-                        </HStack>
-                        <FormControl id="userName" p={1}>
-                            <Stack direction={['column', 'row']} spacing={6}>
-                                <Center>
-                                    <Avatar size="md"
-                                        src="https://bit.ly/sage-adebayo">
-                                    </Avatar>
-                                </Center>
-                                <Center w="full">
-                                    <Button w="full">Change Icon</Button>
-                                </Center>
+                            <FormControl id="password" isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <InputGroup>
+                                    <Input type={showPassword ? 'text' : 'password'}
+                                        name="password"
+                                        onChange={handleOnChange}
+                                        value={inputs.password}
+                                    />
+                                    <InputRightElement h={'full'}>
+                                        <Button
+                                            variant={'ghost'}
+                                            onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <HStack>
+                                <FormControl isRequired>
+                                    <FormLabel>Role</FormLabel>
+                                    <Select
+                                        placeholder='Role'
+                                        name="role"
+                                        onChange={(e) => handleOnChange(e)}
+                                        value={inputs.role}
+                                    >
+                                        <option value='student'>Student</option>
+                                        <option value='recruiter'>Recruiter</option>
+                                    </Select>
+
+                                </FormControl>
+                            </HStack>
+                            <FormControl id="userName" p={1}>
+                                <Stack direction={['column', 'row']} spacing={6}>
+                                    <Center>
+                                        <Avatar size="md"
+                                            src={imageUrl && imageUrl}>
+                                        </Avatar>
+                                    </Center>
+                                    <Center w="full">
+                                        <Button w="full"
+                                            onClick={() => imageRef.current.click()}>Change Icon</Button>
+                                        <Input type='file'
+                                            onChange={handleImageOnChange}
+                                            ref={imageRef}
+                                            hidden
+                                        />
+                                    </Center>
+                                </Stack>
+                            </FormControl>
+                            <Stack spacing={10} pt={2}>
+                                <Button
+                                    isLoading={loading}
+                                    type='submit'
+                                    loadingText="Submitting"
+                                    size="lg"
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}>
+                                    Sign up
+                                </Button>
                             </Stack>
-                        </FormControl>
-                        <Stack spacing={10} pt={2}>
-                            <Button
-                                loadingText="Submitting"
-                                size="lg"
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}>
-                                Sign up
-                            </Button>
+                            <Stack pt={6}>
+                                <Text align={'center'}>
+                                    Already a user? <Link to={'/login'}>Login</Link>
+                                </Text>
+                            </Stack>
                         </Stack>
-                        <Stack pt={6}>
-                            <Text align={'center'}>
-                                Already a user? <Link to={'/login'}>Login</Link>
-                            </Text>
-                        </Stack>
-                    </Stack>
-                </Box>
+                    </Box>
+                </form>
             </Stack>
         </Flex>
-    )
+    );
 }
