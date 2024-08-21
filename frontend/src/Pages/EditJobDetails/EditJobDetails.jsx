fd0d8274e429f1b8ca9b821681c92e2109143fc8
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-21 23:06:27 +0530

    ' Commit Description
    ' Added EditJobDetails Component
    '
    ' Description:
    ' - Implemented `EditJobDetails` component for editing job information.
    ' - Utilized Chakra UI components: `Box`, `Button`, `Flex`, `FormControl`, `FormLabel`, `Heading`, `HStack`, `Input`, `InputGroup`, `Stack`, `useColorModeValue`, `Text`, `InputLeftElement`, `Select`.
    ' - Integrated icons using `FaBuilding`, `MdEditNote`, `IoIosLink`, and `MdLocationPin` for form fields.
    ' - Added fields for job name, description, number of positions, location, requirements, salary, experience, company selection, and job type.
    ' - Styled the form with Chakra UI and configured the form submission button.
    '
    ' Changes:
    ' - Created `EditJobDetails.js` with a form layout for editing job details.
    ' - Included validation for required fields and placeholders for input fields.
    ' - Added dropdowns for selecting company and job type.
    ' - Configured button to update job details.
    '
    ' Component Functionality:
    ' - Provides a form for updating existing job details with various input fields.
    ' - Includes dropdowns for selecting company and job type.
    ' - Configured button to submit the updated job information.

---
+++ frontend/src/Pages/EditJobDetails/EditJobDetails.jsx
@@ -0,0 +1,180 @@
+import { Box, Button, Flex, FormControl, FormLabel, Heading, HStack, Input, InputGroup, Stack, useColorModeValue, Text, InputLeftElement, Select } from '@chakra-ui/react'
+import { FaBuilding } from "react-icons/fa";
+import { MdEditNote } from "react-icons/md";
+import { IoIosLink } from "react-icons/io";
+import { MdLocationPin } from "react-icons/md";
+
+const EditJobDetails = () => {
+    return (
+        <>
+            <Flex align={'center'} justify={'center'}>
+                <Stack spacing={7} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'2xl'} textAlign={'center'}>
+                            Edit Job Details
+                        </Heading>
+                    </Stack>
+                    <form>
+                        <Box
+                            rounded={'lg'}
+                            bg={useColorModeValue('white', 'gray.900')}
+                            boxShadow={'lg'}
+                            p={8}>
+                            <Stack spacing={4}>
+                                <Box>
+                                    <FormControl id="companyName" isRequired>
+                                        <FormLabel>Job Name</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <FaBuilding color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type="text"
+                                                name="jobname"
+                                                placeholder='Enter job name'
+                                            />
+                                        </InputGroup>
+                                    </FormControl>
+                                </Box>
+                                <Box>
+                                    <FormControl id="Description" isRequired>
+                                        <FormLabel>Description</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <MdEditNote color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type="type"
+                                                name="description"
+                                                placeholder='description'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                </Box>
+                                <HStack>
+                                    <FormControl id="website" isRequired>
+                                        <FormLabel>No of Position</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <IoIosLink color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type="number"
+                                                name="postion"
+                                                placeholder='No of postion'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                    <FormControl id="password" isRequired>
+                                        <FormLabel>Location</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <MdLocationPin color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type='text'
+                                                name="location"
+                                                placeholder='company location'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                </HStack>
+                                <HStack>
+                                    <FormControl id="website" isRequired>
+                                        <FormLabel>Requirements</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <IoIosLink color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type="text"
+                                                name="requirements"
+                                                placeholder='requirements'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                    <FormControl id="password" isRequired>
+                                        <FormLabel display={"flex"}
+                                            alignItems={"center"}
+                                            gap={2}
+                                        >Salary <Text fontSize={"12px"} color={"#c5c5c5"}>(in LPA)</Text></FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <MdLocationPin color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type='text'
+                                                name="Salary in LPA"
+                                                placeholder='Salary in LPA'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                </HStack>
+                                <HStack>
+                                    <FormControl id="website" isRequired>
+                                        <FormLabel display={"flex"}
+                                            alignItems={"center"}
+                                            gap={2}
+                                        >Experience<Text fontSize={"12px"} color={"#c5c5c5"}>(in years)</Text></FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement>
+                                                <IoIosLink color='#c5c5c5' />
+                                            </InputLeftElement>
+                                            <Input focusBorderColor='#48bb78' type="text"
+                                                name="Experience"
+                                                placeholder='Experience'
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                    <FormControl id="selectCompany" isRequired>
+                                        <FormLabel display={"flex"}
+                                            alignItems={"center"}
+                                            gap={2}
+                                        >Select Company</FormLabel>
+                                        <Select
+                                            placeholder='Select Company'
+                                            name="company"
+                                        >
+                                            <option value='Google'>Google</option>
+                                            <option value='VisYad'>VisYad</option>
+                                        </Select>
+
+                                    </FormControl>
+                                </HStack>
+                                <FormControl id="userName" p={1}>
+                                    <FormLabel display={"flex"}
+                                        alignItems={"center"}
+                                        gap={2}
+                                    >Job Type</FormLabel>
+                                    <Select
+                                        placeholder='Select Job Type'
+                                        name="jobType"
+                                    >
+                                        <option value='Part Time'>Part Time</option>
+                                        <option value='Full Time'>Full Time</option>
+                                    </Select>
+                                </FormControl>
+                                <Stack spacing={10} pt={2}>
+                                    <Button
+                                        type='submit'
+                                        loadingText="Submitting"
+                                        size="lg"
+                                        bg={'#48bb78'}
+                                        color={'white'}
+                                        _hover={{
+                                            bg: '#22543d',
+                                        }}>
+                                        Update Job
+                                    </Button>
+                                </Stack>
+
+                            </Stack>
+                        </Box>
+                    </form>
+                </Stack>
+            </Flex>
+        </>
+    )
+}
+
+export default EditJobDetails
