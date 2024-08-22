6111a0f89dd1a09b1c58b92c522aa975bd23e11d
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-22 18:33:43 +0530

    Commit Description: Implement updateCompanyDetails action for updating company details
    ' This commit introduces the updateCompanyDetails action, which is responsible for
    ' handling the process of updating company details through an API call. It dispatches
    ' different actions based on the success or failure of the request.
    
    ' The updateCompanyDetails function performs the following steps:
    ' 1. Dispatches UPDATE_COMPANY_DETAILS_REQUEST to indicate the start of the update process.
    ' 2. Sends a PUT request to the API endpoint '/api/v1/company/update/{id}'
    '    with the company id and updated data as payload.
    ' 3. If the response indicates success, it dispatches UPDATE_COMPANY_DETAILS_SUCCESS
    '    with the response data.
    ' 4. If the response indicates failure, it dispatches UPDATE_COMPANY_DETAILS_FAIL
    '    with the error message.
    ' 5. Catches any errors during the request and dispatches UPDATE_COMPANY_DETAILS_FAIL
    '    with the appropriate error message.

--- frontend/src/redux/actions/companyAction.jsx
+++ frontend/src/redux/actions/companyAction.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios'
-import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_SUCEESS } from '../constants/companyConstant';
+import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_SUCEESS, UPDATE_COMPANY_DETAILS_FAIL, UPDATE_COMPANY_DETAILS_REQUEST, UPDATE_COMPANY_DETAILS_SUCEESS } from '../constants/companyConstant';
+
+
+// REGISTER COMPANY
 const createCompany = (name, location) => async (dispatch) => {
     try {
         dispatch({ type: CREATE_NEW_COMPANY_REQUEST })
@@ -32,11 +35,45 @@ const createCompany = (name, location) => async (dispatch) => {
     }
 }
 
-const clearErrors = () => async () => {
+
+// UPDATE COMPANY DETAILS
+const updateCompanyDetails = (id, companyData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_COMPANY_DETAILS_REQUEST })
+        const response = await axios.put(
+            `/api/v1/company/update/${id}`, // company id
+            companyData,
+            { headers: { "Content-Type": "application/json" } },
+        );
+        if (response.data.success) {
+            dispatch({
+                type: UPDATE_COMPANY_DETAILS_SUCEESS,
+                payload: response.data,
+            })
+        }
+        else {
+            dispatch({
+                type: UPDATE_COMPANY_DETAILS_FAIL,
+                payload: response.data.message,
+            });
+        }
+    } catch (error) {
+        dispatch({
+            type: UPDATE_COMPANY_DETAILS_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+    }
+}
+
+
+const clearErrors = () => async (dispatch) => {
     dispatch({ type: CLEAR_ERRORS });
 }
 
 export {
     createCompany,
+    updateCompanyDetails,
     clearErrors,
 }
