0ea7650079b9f2e8cf8a9513f0984cbda18803f3
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-22 18:32:53 +0530

    Commit Description: Implement updateCompanyReducer for handling company details update actions
    ' This commit introduces the updateCompanyReducer function, which manages the state
    ' related to updating company details based on dispatched actions. The reducer updates
    ' the state according to the action type and returns the appropriate state object.
    
    ' The reducer handles the following actions:
    ' - UPDATE_COMPANY_DETAILS_REQUEST: Sets the loading state to true to indicate
    '   that the update request is in progress.
    ' - UPDATE_COMPANY_DETAILS_SUCCESS: Updates the state with the success message
    '   and sets isUpdated to true upon successful update.
    ' - UPDATE_COMPANY_DETAILS_FAIL: Updates the state with the error payload when
    '   the update fails.
    ' - UPDATE_COMPANY_DETAILS_RESET: Resets the isUpdated state to false.
    ' - CLEAR_ERRORS: Clears any error messages in the state.
    
    ' The reducer also includes a default case to handle any unspecified actions
    ' and return the current state.

--- frontend/src/redux/reducers/companyReducer.jsx
+++ frontend/src/redux/reducers/companyReducer.jsx
@@ -1,4 +1,4 @@
-import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_RESET, CREATE_NEW_COMPANY_SUCEESS } from "../constants/companyConstant";
+import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_RESET, CREATE_NEW_COMPANY_SUCEESS, UPDATE_COMPANY_DETAILS_FAIL, UPDATE_COMPANY_DETAILS_REQUEST, UPDATE_COMPANY_DETAILS_RESET, UPDATE_COMPANY_DETAILS_SUCEESS } from "../constants/companyConstant";
 
 const companyReducer = (state = { company: {} }, action) => {
     switch (action.type) {
@@ -36,4 +36,39 @@ const companyReducer = (state = { company: {} }, action) => {
     }
 }
 
-export { companyReducer }
+const updateCompanyReducer = (state = {}, action) => {
+    switch (action.type) {
+        case UPDATE_COMPANY_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case UPDATE_COMPANY_DETAILS_SUCEESS:
+            return {
+                ...state,
+                loading: false,
+                isUpdated: true,
+                message: action.payload.message,
+            }
+        case UPDATE_COMPANY_DETAILS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        case UPDATE_COMPANY_DETAILS_RESET:
+            return {
+                ...state,
+                isUpdated: false,
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            }
+        default:
+            return state;
+    }
+}
+
+export { companyReducer, updateCompanyReducer }
