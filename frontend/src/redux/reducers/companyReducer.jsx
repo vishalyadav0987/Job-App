e1c6d75e5e0a3c2cdd7682a58531840735749363
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-22 19:18:35 +0530

    ' Commit Description: Implement singalCompanyReducer for handling fetching company details actions
    ' This commit introduces the singalCompanyReducer function, which manages the state
    ' related to fetching a single company's details based on dispatched actions.
    ' The reducer updates the state according to the action type and returns the appropriate state object.
    
    ' The reducer handles the following actions:
    ' - GET_COMPANY_DETAILS_REQUEST: Sets the loading state to true to indicate
    '   that the fetch request is in progress.
    ' - GET_COMPANY_DETAILS_SUCCESS: Updates the state with the fetched company data
    '   upon successful retrieval.
    ' - GET_COMPANY_DETAILS_FAIL: Updates the state with the error payload when
    '   the fetch fails.
    ' - CLEAR_ERRORS: Clears any error messages in the state.
    
    ' The reducer also includes a default case to handle any unspecified actions
    ' and return the current state.

--- frontend/src/redux/reducers/companyReducer.jsx
+++ frontend/src/redux/reducers/companyReducer.jsx
@@ -1,4 +1,4 @@
-import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_RESET, CREATE_NEW_COMPANY_SUCEESS, UPDATE_COMPANY_DETAILS_FAIL, UPDATE_COMPANY_DETAILS_REQUEST, UPDATE_COMPANY_DETAILS_RESET, UPDATE_COMPANY_DETAILS_SUCEESS } from "../constants/companyConstant";
+import { CLEAR_ERRORS, CREATE_NEW_COMPANY_FAIL, CREATE_NEW_COMPANY_REQUEST, CREATE_NEW_COMPANY_RESET, CREATE_NEW_COMPANY_SUCEESS, GET_COMPANY_DETAILS_FAIL, GET_COMPANY_DETAILS_REQUEST, GET_COMPANY_DETAILS_SUCEESS, UPDATE_COMPANY_DETAILS_FAIL, UPDATE_COMPANY_DETAILS_REQUEST, UPDATE_COMPANY_DETAILS_RESET, UPDATE_COMPANY_DETAILS_SUCEESS } from "../constants/companyConstant";
 
 const companyReducer = (state = { company: {} }, action) => {
     switch (action.type) {
@@ -71,4 +71,34 @@ const updateCompanyReducer = (state = {}, action) => {
     }
 }
 
-export { companyReducer, updateCompanyReducer }
+
+const singalCompanyReducer = (state = { company: {} }, action) => {
+    switch (action.type) {
+        case GET_COMPANY_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case GET_COMPANY_DETAILS_SUCEESS:
+            return {
+                loading: false,
+                company: action.payload.data,
+            }
+        case GET_COMPANY_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            }
+
+        default:
+            return state;
+    }
+}
+
+export { companyReducer, updateCompanyReducer, singalCompanyReducer }
