155228e35097615d051cbb33d335c504a30d3a18
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-22 19:59:53 +0530

    Commit Description: Combine multiple reducers into a root reducer
    ' This commit introduces the root reducer that combines various feature reducers
    ' into a single reducer function using combineReducers from Redux. This structure
    ' allows for better organization and management of the application state.
    
    ' The combined reducers include:
    ' - user: Manages user-related state through userReducer.
    ' - newJob: Manages the state for creating new jobs through jobReducer.
    ' - updateProfile: Manages the state for updating user profiles through profileReducer.
    ' - newCompany: Manages the state for creating new companies through companyReducer.
    ' - updateCompany: Manages the state for updating company details through updateCompanyReducer.
    ' - singalCompanyById: Manages the state for fetching a single company's details through singalCompanyReducer.
    ' - getAllCompany: Manages the state for fetching all companies.

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,10 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { thunk } from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { userReducer } from './redux/reducers/userReducer';
+import { profileReducer, userReducer } from './redux/reducers/userReducer';
+import jobReducer from './redux/reducers/jobReducer';
+import { companyReducer, getAllCompany, singalCompanyReducer, updateCompanyReducer } from './redux/reducers/companyReducer';
 
 const reducer = combineReducers({
     user: userReducer,
+    newJob: jobReducer,
+    updateProfile: profileReducer,
+    newCompany: companyReducer,
+    updateCompany: updateCompanyReducer,
+    singalCompanyById: singalCompanyReducer,
+    getAllCompany,
 });
 
 const middleware = [thunk];
