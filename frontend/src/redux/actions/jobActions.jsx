8a308467e97338000b246d61e13ae9e0bf5121ff
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-23 09:15:40 +0530

    Commit Description: Implement getAllJobsOfAdmin action for fetching all jobs for admin
    ' This commit introduces the getAllJobsOfAdmin action, which is responsible for
    ' handling the process of fetching all jobs for the admin through an API call.
    ' It dispatches different actions based on the success or failure of the request.
    
    ' The getAllJobsOfAdmin function performs the following steps:
    ' 1. Dispatches GET_ALL_JOB_PRESENT_ADMIN_REQUEST to indicate the start of the fetch process.
    ' 2. Sends a GET request to the API endpoint '/api/v1/job/getall' to retrieve all jobs.
    ' 3. If the response indicates success, it dispatches GET_ALL_JOB_PRESENT_ADMIN_SUCCESS
    '    with the response data.
    ' 4. If the response indicates failure, it dispatches GET_ALL_JOB_PRESENT_ADMIN_FAIL
    '    with the error message.
    ' 5. Catches any errors during the request and dispatches GET_ALL_JOB_PRESENT_ADMIN_FAIL
    '    with the appropriate error message.

--- frontend/src/redux/actions/jobActions.jsx
+++ frontend/src/redux/actions/jobActions.jsx
@@ -1,9 +1,9 @@
-import { CLEAR_ERRORS, POST_NEW_JOB_FAIL, POST_NEW_JOB_REQUEST, POST_NEW_JOB_SUCEESS } from "../constants/jobConstants"
+import { CLEAR_ERRORS, GET_ALL_JOB_PRESENT_ADMIN_FAIL, GET_ALL_JOB_PRESENT_ADMIN_REQUEST, GET_ALL_JOB_PRESENT_ADMIN_SUCEESS, POST_NEW_JOB_FAIL, POST_NEW_JOB_REQUEST, POST_NEW_JOB_SUCEESS } from "../constants/jobConstants"
 import axios from 'axios'
 
 
 // POST JOB -- ADMIN
-const postNewJob = (jobData) => async (dispatch) => { 
+const postNewJob = (jobData) => async (dispatch) => {
     try {
         dispatch({ type: POST_NEW_JOB_REQUEST });
         const response = await axios.post(
@@ -34,9 +34,38 @@ const postNewJob = (jobData) => async (dispatch) => {
     }
 }
 
+// GET ALL JOBS OF ADMIN CREATED
+const getAllJobsOfAdmin = () => async (dispatch) => {
+    try {
+        dispatch({ type: GET_ALL_JOB_PRESENT_ADMIN_REQUEST })
+        const response = await axios.get(
+            '/api/v1/job/getall' // of Admin
+        );
+        if (response.data.success) {
+            dispatch({
+                type: GET_ALL_JOB_PRESENT_ADMIN_SUCEESS,
+                payload: response.data,
+            });
+        }
+        else {
+            dispatch({
+                type: GET_ALL_JOB_PRESENT_ADMIN_FAIL,
+                payload: response.data.message,
+            });
+        }
+    } catch (error) {
+        dispatch({
+            type: GET_ALL_JOB_PRESENT_ADMIN_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+    }
+}
+
 // clearing Error
 const clearError = () => async (dispatch) => {
     dispatch({ type: CLEAR_ERRORS });
 }
 
-export { postNewJob, clearError }
+export { postNewJob, getAllJobsOfAdmin, clearError }
