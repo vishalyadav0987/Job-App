7e91cdbed1dce3ca6ef4617b8e5f62b41f1e5fbd
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-23 07:31:21 +0530

    Commit Description: Implement jobReducer for handling job posting actions
    ' This commit introduces the jobReducer function, which manages the state
    ' related to posting a new job based on dispatched actions. The reducer updates
    ' the state according to the action type and returns the appropriate state object.
    
    ' The reducer handles the following actions:
    ' - POST_NEW_JOB_REQUEST: Sets the loading state to true to indicate
    '   that the job posting request is in progress.
    ' - POST_NEW_JOB_SUCCESS: Updates the state with the success status, job data,
    '   and message upon successful job posting.
    ' - POST_NEW_JOB_FAIL: Updates the state with the error payload when
    '   the job posting fails.
    ' - POST_NEW_JOB_RESET: Resets the success state to false.
    ' - CLEAR_ERRORS: Clears any error messages in the state.
    
    ' The reducer also includes a default case to handle any unspecified actions
    ' and return the current state.

---
+++ frontend/src/redux/reducers/jobReducer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { POST_NEW_JOB_FAIL, POST_NEW_JOB_REQUEST, POST_NEW_JOB_RESET, POST_NEW_JOB_SUCEESS } from '../constants/jobConstants';
+import { CLEAR_ERRORS } from '../constants/userConstants';
+
+const jobReducer = (state = { Job: {} }, action) => {
+    switch (action.type) {
+        case POST_NEW_JOB_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case POST_NEW_JOB_SUCEESS:
+            return {
+                loading: false,
+                success: action.payload.success,
+                Job: action.payload.data,
+                message: action.payload.message,
+            }
+        case POST_NEW_JOB_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        case POST_NEW_JOB_RESET:
+            return {
+                ...state,
+                success: false,
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            }
+        default:
+            return state;
+    }
+}
+
+export default jobReducer
