c4305f2bc4b3359b8c7914624b247f78a3055e62
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-25 15:48:15 +0530

    Commit Description: Implement job applicants fetching component with error handling and loading state
    ' This commit introduces a React component that fetches and displays applicants for a specific job
    ' based on the job ID from the URL parameters. The component utilizes hooks to manage state, handle
    ' side effects, and dispatch actions to the Redux store.
    
    ' Key features of the component include:
    ' 1. Using useColorMode to adjust the component's styling based on the current color mode.
    ' 2. Using useParams to extract the job ID from the URL.
    ' 3. Using useDispatch to dispatch actions to the Redux store.
    ' 4. Using useSelector to access the loading state, error messages, and applications from the Redux store.
    ' 5. Implementing useEffect to handle side effects, such as fetching applicants and displaying
    '    error messages using toast notifications.
    ' 6. Displaying a loading spinner while the application data is being fetched.

--- frontend/src/Pages/Admin/Applicants/Applicants.jsx
+++ frontend/src/Pages/Admin/Applicants/Applicants.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     Table,
     Thead,
@@ -22,26 +22,60 @@ import {
     Text,
     Divider,
     Link as ChakraLink,
-    Heading
+    Heading,
+    Spinner,
+    Button
 } from '@chakra-ui/react'
-import {  BsThreeDots } from "react-icons/bs";
+import { BsThreeDots } from "react-icons/bs";
 import { FcCancel } from "react-icons/fc";
 import { FcAcceptDatabase } from "react-icons/fc";
+import toast from 'react-hot-toast';
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom';
+import { clearErrors, getAllApplicantOnJob } from '../../../redux/actions/applicationAction';
 
 const Applicants = () => {
-    const { colorMode } = useColorMode()
+    const { colorMode } = useColorMode();
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const {
+        loading: applicationLoading,
+        error: applicationError,
+        applications // not direction application array it job array in it application
+    } = useSelector(state => state.application);
+
+    useEffect(() => {
+        if (applicationError) {
+            toast.error(applicationError);
+            dispatch(clearErrors());
+        }
+        dispatch(getAllApplicantOnJob(id));
+    }, [dispatch, applicationError, id]);
+
+    if (applicationLoading) {
+        return (
+
+            <Flex w={"100vw"} height={"60vh"} alignItems={"center"} justifyContent={"center"}>
+                <Spinner size={"xl"} />
+            </Flex>
+
+        )
+    }
+
     return (
         <>
             <Box w={"full"} py={12} minH={"60vh"}>
                 <Box w={"80%"} margin={"0 auto"}>
                     <Flex alignItems={"center"} justifyContent={"space-between"} mb={12}>
                         <Stack spacing={3}>
-                            <Heading size={"md"}>Applicants (1)</Heading>
+                            <Heading size={"md"}>
+                                Applicants ({applications && applications.applications?.length})
+                            </Heading>
                         </Stack>
                     </Flex>
                     <TableContainer>
                         <Table variant="simple">
-                            <TableCaption>Imperial to metric conversion factors</TableCaption>
+                            <TableCaption>A list of Applicant applied on jobs</TableCaption>
                             <Thead>
                                 <Tr borderBottom="2px solid #3c3c3c">
                                     <Th>Full Name</Th>
@@ -50,50 +84,88 @@ const Applicants = () => {
                                     <Th>Resume</Th>
                                     <Th>Date</Th>
                                     <Th>Action</Th>
+                                    <Th textAlign={"center"}>Status</Th>
                                 </Tr>
                             </Thead>
                             <Tbody>
-                                <Tr borderBottom="1px solid #3c3c3c">
-                                    <Td>Vishal Yadav</Td>
-                                    <Td>Vishalyadav@gmail.com</Td>
-                                    <Td>9667225460</Td>
-                                    <Td><ChakraLink href='resume.png'
-                                        color={"#48bb78"}
-                                    >resume.png</ChakraLink></Td>
-                                    <Td>24-08-25</Td>
-                                    <Td>
-                                        <Popover>
-                                            <PopoverTrigger>
-                                                <IconButton
-                                                    icon={<BsThreeDots />}
-                                                />
-                                            </PopoverTrigger>
-                                            <PopoverContent>
-                                                <PopoverArrow />
-                                                <PopoverCloseButton />
-                                                <PopoverBody>
-                                                    <Box>
+                                {
+                                    applications && applications.applications?.length > 0 &&
+                                    applications.applications?.map((application) => {
+                                        return (
+                                            <Tr borderBottom="1px solid #3c3c3c">
+                                                <Td>
+                                                    {applications && application.applicantId?.fullname}
+                                                </Td>
+                                                <Td>
+                                                    {applications && application.applicantId?.email}
+                                                </Td>
+                                                <Td>
+                                                    {
+                                                        applications &&
+                                                        application.applicantId?.phoneNumber
+                                                    }
+                                                </Td>
+                                                <Td><ChakraLink
+                                                    href={
+                                                        applications &&
+                                                        application.applicantId?.profile?.profilePic
+                                                    }
+                                                    color={"#48bb78"}
+                                                >resume.png</ChakraLink></Td>
+                                                <Td>
+                                                    {applications &&
+                                                        application.applicantId?.createdAt
+                                                            .substring(0, 10)
+                                                            .split("-")
+                                                            .reverse()
+                                                            .join("-")
+                                                    }
+                                                </Td>
+                                                <Td>
+                                                    <Popover>
+                                                        <PopoverTrigger>
+                                                            <IconButton
+                                                                icon={<BsThreeDots />}
+                                                            />
+                                                        </PopoverTrigger>
+                                                        <PopoverContent>
+                                                            <PopoverArrow />
+                                                            <PopoverCloseButton />
+                                                            <PopoverBody>
+                                                                <Box>
 
-                                                        <Flex alignItems={"center"} gap={2} 
-                                                        cursor={"pointer"}>
-                                                            <FcAcceptDatabase />
-                                                            <Text>Accepted</Text>
-                                                        </Flex>
+                                                                    <Flex alignItems={"center"} gap={2}
+                                                                        cursor={"pointer"}>
+                                                                        <FcAcceptDatabase />
+                                                                        <Text>Accepted</Text>
+                                                                    </Flex>
 
-                                                        <Divider mb={2} mt={2}></Divider>
+                                                                    <Divider mb={2} mt={2}></Divider>
 
-                                                        <Flex alignItems={"center"} gap={2}
-                                                         cursor={"pointer"}>
-                                                            <FcCancel />
-                                                            <Text>Rejected</Text>
-                                                        </Flex>
+                                                                    <Flex alignItems={"center"} gap={2}
+                                                                        cursor={"pointer"}>
+                                                                        <FcCancel />
+                                                                        <Text>Rejected</Text>
+                                                                    </Flex>
 
-                                                    </Box>
-                                                </PopoverBody>
-                                            </PopoverContent>
-                                        </Popover>
-                                    </Td>
-                                </Tr>
+                                                                </Box>
+                                                            </PopoverBody>
+                                                        </PopoverContent>
+                                                    </Popover>
+                                                </Td>
+                                                <Td textAlign={"center"}>
+                                                    <Button bg={colorMode === "light"
+                                                        ? "#edf2f7" : "#3c3c3c"} color={
+                                                            application?.status === "pending" ? "#ff5733" : "#48bb78"
+                                                        }
+                                                    >
+                                                        {applications && application.status}
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })
+                                }
 
                             </Tbody>
                         </Table>
