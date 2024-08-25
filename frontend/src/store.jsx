a60a8437eacba390eb7f4c3b845fa9603f122ee0
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-25 18:25:08 +0530

    Commit Description: Configure root reducer for job-related actions
    ' This commit introduces the configuration of the root reducer that combines
    ' multiple reducers related to job management and applications. This setup
    ' allows for centralized state management for various job-related functionalities
    ' within the application.
    
    ' Key features of the root reducer configuration include:
    ' 1. Combining the getAllJobs reducer to manage the state of all job listings.
    ' 2. Including the updateJobDetails reducer to handle updates to job details.
    ' 3. Integrating the applyJobReducer to manage job application states.
    ' 4. Adding the applicationReducer to handle state related to job applications.
    ' 5. Including the updateJobStatusReducer to manage updates to job statuses.

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { thunk } from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { profileReducer, userReducer } from './redux/reducers/userReducer';
-import jobReducer from './redux/reducers/jobReducer';
+import { applyJobReducer, profileReducer, userReducer } from './redux/reducers/userReducer';
+import { jobReducer, getAllJobs, updateJobDetaailsReducer } from './redux/reducers/jobReducer';
 import { companyReducer, getAllCompany, singalCompanyReducer, updateCompanyReducer } from './redux/reducers/companyReducer';
+import { applicationReducer, updateJobStatusReducer } from './redux/reducers/applicationReducer';
 
 const reducer = combineReducers({
     user: userReducer,
@@ -13,6 +14,11 @@ const reducer = combineReducers({
     updateCompany: updateCompanyReducer,
     singalCompanyById: singalCompanyReducer,
     getAllCompany,
+    adminAllJobs: getAllJobs,
+    updateJobDetails: updateJobDetaailsReducer,
+    jobApplication: applyJobReducer,
+    application: applicationReducer,
+    updateJobStatus: updateJobStatusReducer,
 });
 
 const middleware = [thunk];
