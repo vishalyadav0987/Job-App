2fa494b31c3effffb66c344d46f165a05dd03f83
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-25 14:36:05 +0530

    Commit Description: Implement actions for fetching applicant applications and clearing errors
    ' This commit introduces the getApplicatonOfApplicant action creator, which is responsible for
    ' handling the process of fetching all applications of an applicant from the API.
    ' It dispatches different actions based on the success or failure of the request.
    ' Additionally, it includes a clearErrors action creator to reset error states.
    
    ' The getApplicatonOfApplicant function performs the following steps:
    ' 1. Dispatches GET_ALL_APPLICATION_OF_APPLICANT_REQUEST to indicate the start of the fetch process.
    ' 2. Sends a GET request to the API endpoint '/api/v1/application/get' to retrieve all applications.
    ' 3. If the response indicates success, it dispatches GET_ALL_APPLICATION_OF_APPLICANT_SUCCESS
    '    with the response data.
    ' 4. If the response indicates failure, it dispatches GET_ALL_APPLICATION_OF_APPLICANT_FAIL
    '    with the error message.
    ' 5. Catches any errors during the request and dispatches GET_ALL_APPLICATION_OF_APPLICANT_FAIL
    '    with the appropriate error message.
    
    ' The clearErrors function dispatches CLEAR_ERRORS to reset any error messages in the state.

---
+++ frontend/src/redux/actions/applicationAction.jsx
@@ -0,0 +1,35 @@
+import { CLEAR_ERRORS, GET_ALL_APPLICATION_OF_APPLICANT_FAIL, GET_ALL_APPLICATION_OF_APPLICANT_REQUEST, GET_ALL_APPLICATION_OF_APPLICANT_SUCEESS } from "../constants/applicationConstant"
+import axios from 'axios'
+
+const getApplicatonOfApplicant = () => async (dispatch) => {
+    try {
+        dispatch({ type: GET_ALL_APPLICATION_OF_APPLICANT_REQUEST });
+        const response = await axios.get(
+            '/api/v1/application/get'
+        );
+        if (response.data.success) {
+            dispatch({
+                type: GET_ALL_APPLICATION_OF_APPLICANT_SUCEESS,
+                payload: response.data,
+            })
+        }
+        else {
+            dispatch({
+                type: GET_ALL_APPLICATION_OF_APPLICANT_FAIL,
+                payload: response.data.message,
+            });
+        }
+    } catch (error) {
+        dispatch({
+            type: GET_ALL_APPLICATION_OF_APPLICANT_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        });
+    }
+}
+
+const clearErrors = () => async (dispatch) => {
+    dispatch({ type: CLEAR_ERRORS })
+}
+export { getApplicatonOfApplicant, clearErrors };
