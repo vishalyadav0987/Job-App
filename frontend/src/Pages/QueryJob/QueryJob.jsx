f4ab0a83452d1d935c3201d74646c8d994948925
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-08-26 09:32:54 +0530

    commit description: -Change the styling of searchQuery job page add layout design as grid.

--- frontend/src/Pages/QueryJob/QueryJob.jsx
+++ frontend/src/Pages/QueryJob/QueryJob.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getAllJobsForUser } from '../../redux/actions/jobActions';
-import { Box, Button, Flex, Heading, Spinner } from '@chakra-ui/react';
+import { Box, Button, Flex, Grid, Heading, Spinner } from '@chakra-ui/react';
 import JobCard from '../../Components/JobCard/JobCard';
 import { Link } from 'react-router-dom';
 import { FaArrowAltCircleLeft } from 'react-icons/fa';
@@ -17,7 +17,8 @@ const QueryJob = () => {
         const filterJob = jobs && jobs?.length > 0 &&
             jobs.filter((job) => {
                 return job?.title?.toLowerCase().includes(searchQueryuery.toLowerCase()) ||
-                    job?.comapanyId?.name?.toLowerCase().includes(searchQueryuery.toLowerCase());
+                    job?.comapanyId?.name?.toLowerCase().includes(searchQueryuery.toLowerCase()) ||
+                    job?.description.toLowerCase().includes(searchQueryuery.toLowerCase());
             });
         setFilteredJob(filterJob);
     }, [searchQueryuery, jobs])
@@ -35,27 +36,39 @@ const QueryJob = () => {
         <>
             <Box px={16} py={10} minH={"55vh"}>
                 <Heading mb={10} size={"md"}>Search Job ({filteredJob && filteredJob?.length || "0"})</Heading>
-                {
-                    (filteredJob && filteredJob?.length > 0) ?
-                        filteredJob.map((job) => {
-                            return (
-                                <JobCard job={job} key={job?._id} />
+                <Grid
+                px={20}
+                    templateColumns={{
+                        base: 'repeat(1, 1fr)',
+                        sm: 'repeat(2, 1fr)',
+                        md: 'repeat(3, 1fr)',
+                        lg: 'repeat(3, 1fr)'
+                    }}
+                    gap={4}
+                    mt={{ base: 8, lg: 0 }}
+                >
+                    {
+                        (filteredJob && filteredJob?.length > 0) ?
+                            filteredJob.map((job) => {
+                                return (
+                                    <JobCard job={job} key={job?._id} />
+                                )
+                            }) : (
+
+                                <>
+                                    <Heading mb={10} size={"md"}>
+                                        Job not found!.
+                                    </Heading>
+                                    <Link to={'/'}>
+                                        <Button display={"flex"} alignItems={"center"} position={"absolute"} top={"30%"} left={"9%"}>
+                                            <FaArrowAltCircleLeft style={{ marginRight: "8px" }} />
+                                            Back To Home</Button>
+                                    </Link>
+                                </>
+
                             )
-                        }) : (
-
-                            <>
-                                <Heading mb={10} size={"md"}>
-                                    Job not found!.
-                                </Heading>
-                                <Link to={'/'}>
-                                    <Button display={"flex"} alignItems={"center"}>
-                                        <FaArrowAltCircleLeft style={{marginRight:"8px"}}/> 
-                                        Back To Home</Button>
-                                </Link>
-                            </>
-
-                        )
-                }
+                    }
+                </Grid>
             </Box>
         </>
     )
